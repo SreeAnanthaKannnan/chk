f51a2528d43d74ce0b0b9ee489c0a0795b890198
rapidvikramv <vikram.viswanathan@rapidqube.com>
2019-03-03 13:28:46 +0530

    Validation function of Mobile Number added.

---
+++ utils/general.js
@@ -0,0 +1,15 @@
+/**
+ * @author: Vikram Viswanathan
+ * @date: March 03, 2019
+ * @description: This function would valide the UAE mobile number format.
+ * 
+ * @param {*} mobileNumber 
+ */
+function validateMobileNumber(mobileNumber) {
+    //var regexFormat = "/^(?:\+971|00971|971|0)?(?:50|51|52|55|56|58|2|3|4|6|7|9)\d{7}$/m";
+
+    if (mobileNumber.match(/^(?:\+971|00971|0)?(?:50|51|52|55|56|58|2|3|4|6|7|9)\d{7}$/m))
+       return true;
+    else
+        return false;
+}
