0dd4785b3effe01c68730a2d051939c755ea41c4
manojvenkateswararaja <manoj.venkateswararaja@rapidqube.com>
2019-04-05 15:30:04 +0530

    OTP generation has been modified by avoiding 0 in otp

--- utils/otp.js
+++ utils/otp.js
@@ -1,21 +1,18 @@
-
-
-function otpgen(){
-    return new Promise((resolve,reject)=>{
-var otp = "";
-var possible = "0123456789";
-for (var i = 0; i < 4; i++)
-  otp += possible.charAt(Math.floor(Math.random() * possible.length));
-  console.log("otp generated in util" + otp);
-  if(otp){
-      console.log("in if")
- resolve({otp})
-  }
-else{
-     reject({result:err})
-}
-});
+function otpgen() {
+  return new Promise((resolve, reject) => {
+    var otp = "";
+    var possible = "123456789";
+    for (var i = 0; i < 4; i++)
+      otp += possible.charAt(Math.floor(Math.random() * possible.length));
+    console.log("otp generated in util" + otp);
+    if (otp) {
+      console.log("in if");
+      resolve({ otp });
+    } else {
+      reject({ result: err });
     }
-module.exports={
-    otpgen:otpgen
+  });
 }
+module.exports = {
+  otpgen: otpgen
+};
--- utils/otp_generation.js
+++ utils/otp_generation.js
@@ -1,16 +1,14 @@
 function otp_generation() {
-    return new Promise((resolve,reject)=>{
+  return new Promise((resolve, reject) => {
+    var otp = "";
+    var possible = "123456789";
 
-
-        var otp = "";
-        var possible = "0123456789";
-       
-        for (var i = 0; i<4; i++)
-            otp += possible.charAt(Math.floor(Math.random() * possible.length));
-        console.log("otp" + otp);
-        return resolve (otp)
-    })
+    for (var i = 0; i < 4; i++)
+      otp += possible.charAt(Math.floor(Math.random() * possible.length));
+    console.log("otp" + otp);
+    return resolve(otp);
+  });
 }
-module.exports ={
-    otp_generation : otp_generation
-}
+module.exports = {
+  otp_generation: otp_generation
+};
